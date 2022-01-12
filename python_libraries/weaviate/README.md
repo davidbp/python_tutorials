0d876218844e1e7d7e0508b6ed80ce0abc00e1e8
David Buchaca Prats <davidbuchaca@gmail.com>
2022-01-12 15:06:30 +0100

    fix: ensure schema is created only once

---
+++ python_libraries/weaviate/README.md
@@ -0,0 +1,12 @@
+
+To download the .yml for docker run
+```
+curl -o docker-compose.yml "https://configuration.semi.technology/v2/docker-compose/docker-compose.yml?enterprise_usage_collector=false&media_type=none&runtime=docker-compose&weaviate_version=v1.9.0"
+```
+
+Then create a docker instance with 
+
+```
+docker-compose up
+```
+
--- python_libraries/weaviate/toy1.py
+++ python_libraries/weaviate/toy1.py
@@ -1,4 +1,5 @@
 from docarray import Document
+from pprint import pprint
 
 doc_schema = {
     'classes': [
@@ -15,13 +16,20 @@ import weaviate
 
 client = weaviate.Client('http://localhost:8080')
 
-# only do this one-time, otherwise an error schema exist
-# client.schema.create(doc_schema)
+class_exists = False
+for c in client.schema.get()['classes']:
+    if 'JinaDocv' in c['class']:
+        print('\nJinaDocv found\n')
+        pprint(c)
+        class_exists = True
+
+if class_exists == False:
+    print('\nCreating schema with client.schema.create(doc_schema)\n')
+    client.schema.create(doc_schema)
 
 d = Document(embedding=[1, 2, 3])
-
 did = client.data_object.create({'_serialized': d.to_base64()},
-                                class_name='JinaDocv',
-                                vector=d.embedding)
+                                 class_name='JinaDocv',
+                                 vector=d.embedding)
 
 print(Document.from_base64(client.data_object.get_by_id(did)['properties']['_serialized']))
