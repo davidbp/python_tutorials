1c869fb665fefc8fcacce4c05f49121283bc5253
David Buchaca Prats <davidbuchaca@gmail.com>
2023-08-19 22:35:52 +0200

    feat: add tqdm

--- python_tiny_projects/remove_duplicates/remove_duplicates.py
+++ python_tiny_projects/remove_duplicates/remove_duplicates.py
@@ -4,6 +4,7 @@ import hashlib
 import ipyplot
 import numpy as np
 from PIL import Image
+from tqdm import tqdm
 
 
 path = './data_test'
@@ -11,7 +12,7 @@ path = './data_test'
 file_list = os.walk(path)
 
 file_hashes = dict()
-for root,folders,files in file_list:
+for root,folders,files in tqdm(file_list):
     for file in files:
         path = Path(os.path.join(root,file))
         fhash = hashlib.md5(open(path,'rb').read()).hexdigest()
@@ -24,7 +25,7 @@ for root,folders,files in file_list:
 duplicate_file_paths = [x[1] for x in file_hashes.items() if len(x[1])>1]
 
 get_len = lambda x: len(x.stem)
-for duplicates in duplicate_file_paths:
+for duplicates in tqdm(duplicate_file_paths):
     keep_index = np.argmin(list(map(get_len, duplicates)))
     for i, duplicate in enumerate(duplicates):
         if keep_index != i:
